54e7be55efce25989cf3498f40a3ab5222f94d89
chuck hilyard <chilyard@vubiquity.com>
2016-02-18 17:39:02 -0800

    added test

---
+++ lib/aws/test.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'aws-sdk'
+
+require_relative 'AwsCredentials.rb'
+
+
+# REMOVE when done
+#$credentials = Aws::SharedCredentials.new(profile_name: "chilyard")
+#ENV['AWS_REGION'] = "us-west-2"
+
+awsCredentials = AwsCredentials.new()
+
+
+ec2Client = Aws::EC2::Client.new(:credentials => $credentials)
+ec2Resource = Aws::EC2::Resource.new(region: ENV['AWS_REGION'], client: ec2Client)
+
+
+
+ec2Resource.instances.each do |instance|
+	line = instance.instance_id,instance.state.code,instance.state.name
+	line.each { |x| print x,"," };print "\n"
+end
+
+
+
+
+
