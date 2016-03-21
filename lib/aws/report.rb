cff16bc7a94a09d9c7b485917fcd57abd09a9cfc
chilyard <chilyard@example.com>
2016-03-21 07:13:10 -0700

    latest

--- lib/aws/report.rb
+++ lib/aws/report.rb
@@ -19,7 +19,7 @@ instance_array = Array.new()
 
 
 =begin
-:#<struct Aws::EC2::Types::Instance instance_id="i-e763ec3f", image_id="ami-63b25203", state=#<struct Aws::EC2::Types::InstanceState code=16, name="running">, private_dns_name="ip-172-31-35-159.us-west-2.compute.internal", public_dns_name="ec2-54-213-7-249.us-west-2.compute.amazonaws.com", state_transition_reason="", key_name="testkeypair", ami_launch_index=0, product_codes=[], instance_type="t2.micro", launch_time=2016-03-09 06:19:34 UTC, 
+#<struct Aws::EC2::Types::Instance instance_id="i-e763ec3f", image_id="ami-63b25203", state=#<struct Aws::EC2::Types::InstanceState code=16, name="running">, private_dns_name="ip-172-31-35-159.us-west-2.compute.internal", public_dns_name="ec2-54-213-7-249.us-west-2.compute.amazonaws.com", state_transition_reason="", key_name="testkeypair", ami_launch_index=0, product_codes=[], instance_type="t2.micro", launch_time=2016-03-09 06:19:34 UTC, 
 
 
 kernel_id=nil, ramdisk_id=nil, platform=nil, monitoring=#<struct Aws::EC2::Types::Monitoring state="disabled">, subnet_id="subnet-3b1d3e4c", vpc_id="vpc-198bb37c", private_ip_address="172.31.35.159", public_ip_address="54.213.7.249", state_reason=nil, architecture="x86_64", root_device_type="ebs", root_device_name="/dev/xvda", block_device_mappings=[#<struct Aws::EC2::Types::InstanceBlockDeviceMapping device_name="/dev/xvda", ebs=#<struct Aws::EC2::Types::EbsInstanceBlockDevice volume_id="vol-5f35379f", status="attached", attach_time=2016-03-09 06:19:35 UTC, delete_on_termination=true>>], virtualization_type="hvm", instance_lifecycle=nil, spot_instance_request_id=nil, client_token="oKaKA1457504374121", tags=[], 
@@ -35,17 +35,14 @@ attachment=#<struct Aws::EC2::Types::InstanceNetworkInterfaceAttachment attachme
 
 
 
-
 ec2Resource.instances.each do |instance|
-    print ":", instance.instance_id, "\n"
-    print ":", instance.placement.group_name, "\n"
-    print ":", instance.groups#group_name, "\n"
+    print "id:", instance.instance_id, "\n"
+    print "placement:", instance.placement.tenancy, "\n"
+    print "groups:", instance.groups[1], "\n"
 
 	# pop the returned value on the array stack
 	# at the end, dump the array into a file object 
 end
-
-
 
 
 
